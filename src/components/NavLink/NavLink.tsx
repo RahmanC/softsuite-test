5497af627c59cea8b0dc0e4b3d2364bc86da23aa
rhammy <afaraeturahman@gmail.com>
2023-10-23 11:25:33 +0100

    close side menu on click of link

--- src/components/NavLink/NavLink.tsx
+++ src/components/NavLink/NavLink.tsx
@@ -2,8 +2,14 @@ import * as React from "react";
 import { NavLink as Nav } from "react-router-dom";
 import style from "./NavLink.module.scss";
 import { NavLinkProps } from "types";
+import { useDispatch } from "react-redux";
+import { ToggleView } from "redux/slices/util";
 
 const NavLink = ({ icon, text }: NavLinkProps) => {
+  const dispatch: any = useDispatch();
+  const handleClick = () => {
+    dispatch(ToggleView());
+  };
   return (
     <Nav
       to={text.toLocaleLowerCase().replaceAll(" ", "-")}
@@ -12,6 +18,7 @@ const NavLink = ({ icon, text }: NavLinkProps) => {
           ? `${style.side_nav_link} ${style.active}`
           : style.side_nav_link
       }
+      onClick={handleClick}
     >
       {icon && <span>{icon}</span>}
       <span className={style.side_nav_link_text}>{text}</span>
--- src/layout/navbar/Navbar.tsx
+++ src/layout/navbar/Navbar.tsx
@@ -6,30 +6,28 @@ import User from "components/user/User";
 import { ReactComponent as Home } from "assets/svg/house.svg";
 import CustomSwitcher from "components/customSwitch/CustomSwitcher";
 import MobileHamburger from "./MobileHamburger/MobileHamburger";
-import { useSelector } from "react-redux";
 
 const Navbar = () => {
-  const { isMobile } = useSelector((state: any) => state.utils);
-
   return (
     <div className={styles.container}>
       <MobileHamburger />
-      {!isMobile && (
-        <>
-          <div className={styles.container_logo}>
-            <Logo />
-          </div>
-          <div className={styles.container_column2}>
-            <CustomSwitcher
-              icon={<Home />}
-              label="Change Organisation"
-              value="PaperSoft Limited"
-              customClass={styles.container_column2_switch}
-            />
-            <Search placeholder="Search for anything..." />
-          </div>
-        </>
-      )}
+
+      <div className={styles.container_logo}>
+        <Logo />
+      </div>
+      <div className={styles.container_column2}>
+        <CustomSwitcher
+          icon={<Home />}
+          label="Change Organisation"
+          value="PaperSoft Limited"
+          customClass={styles.container_column2_switch}
+        />
+        <Search
+          placeholder="Search for anything..."
+          customClass={styles.container_column2_search}
+        />
+      </div>
+
       <div>
         <User />
       </div>
